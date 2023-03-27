2cf35f1ee4d35beb9a01f04b5e140964af507f63
Peter Akhigbe <akhigbe37@gmail.com>
2023-03-27 09:55:01 +0100

    fix eslint linter error

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import myBookShowList from './modules/myBookShowList.js';
 import myShowContactView from './modules/myShowContactView.js';
 import myAddBookList from './modules/myAddBookList.js';
 import myRedirectButton from './modules/myRedirectButton.js';
-import BookCollection from './modules/BookCollection.js'
+import BookCollection from './modules/BookCollection.js';
 
 import { DateTime } from './modules/luxon.js';
 
